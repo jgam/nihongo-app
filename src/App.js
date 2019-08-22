096a5c516836f91180992daf8809469d2b428b2c
jgam <jgam@alumni.nd.edu>
2019-08-22 22:10:06 +0900

    great division

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Day from './Day';
-import Vocab from './Vocab';
 import Level from './Level';
-
+import Vocab from './Vocab';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      page_state: '1'
+      page_state: '1',
+      level: '',
+      days: '',
+      words: []
     };
+  }
+
+  handleLevel = input_level => {
+    this.setState({
+      level: input_level
+    });
+  }
+
+  handleDays = input_days => {
+    this.setState({
+      days: input_days
+    });
+  }
+
+  handleVocab = (input_vocab, definitions) => {
+    this.setState({
+
+    })
   }
 
   render_views = page_state => {
     if(page_state === '1'){
-      return ;
+      console.log('here')
+      return <Level handleLevel={this.handleLevel}/>
     }
     else if(page_state === '2'){
-      return ;
+      return <Level handleLevel={this.handleLevel}/>
     }
     else if(page_state === '3'){
       return ;
