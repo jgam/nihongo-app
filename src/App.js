63e71c345ddadb7db4181456705e923720b548d7
jgam <jgam@alumni.nd.edu>
2019-08-22 21:46:27 +0900

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Day from './Day';
+import Vocab from './Vocab';
+import Level from './Level';
+
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      page_state: '0'
+      page_state: '1'
     };
   }
+
+  render_views = page_state => {
+    if(page_state === '1'){
+      return ;
+    }
+    else if(page_state === '2'){
+      return ;
+    }
+    else if(page_state === '3'){
+      return ;
+    }
+    else{
+      return 'error occurred';
+    }
+  }
+
   render(){
+    //state logic
+    var current_state = this.state.page_state;
+
+    //return
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload + welcome to nihongo app
           </p>
           <a
             className="App-link"
@@ -26,6 +50,10 @@ class App extends React.Component {
             Learn React
           </a>
         </header>
+        
+        <div className="stateViews">
+          {this.render_views(current_state)}
+        </div>
       </div>
     );
   }
