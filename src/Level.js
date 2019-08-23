25b3e3afb8f78adff412a0eae27972c20b570219
jgam <jgam@alumni.nd.edu>
2019-08-23 14:06:36 +0900

    css style initiated

--- src/Level.js
+++ src/Level.js
@@ -6,13 +6,19 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 const Level = ({ handleButton }) => {
 	return (
 	  <div>
+		<link
+			rel="stylesheet"
+			href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+			crossorigin="anonymous"
+		/>
 		<button onClick={() => handleButton('N5')}>N5</button>
 		<button onClick={() => handleButton('N4')}>N4</button>
 		<button onClick={() => handleButton('N3')}>N3</button>
 		<button onClick={() => handleButton('N2')}>N2</button>
 		<button onClick={() => handleButton('N1')}>N1</button>
 		<ButtonToolbar>
-		<Button variant="outline-primary">Primary</Button>
+			<Button variant="primary">Primary</Button>
 		</ButtonToolbar>
 	  </div>
 	);
