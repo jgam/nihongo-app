6cab1633cd250e1a9230507e81308878d1f51713
jgam <jgam@alumni.nd.edu>
2019-09-03 23:30:50 +0900

    db setup initiated now implement

--- src/App.js
+++ src/App.js
@@ -14,86 +14,10 @@ import Vocab from './Vocab';
 4. Wait for the operation to complete by listening for the right kind of DOM event
 5. Do something with the results
 */
- var request = indexedDB.open('vocabDB', 1);
- var productStore;
 
- request.onsuccess = function(event) {
-     // some sample products data
-     var vocabProgress = [
-         {id: 1, name: 'Red Men T-Shirt', price: '$3.99'},
-         {id: 2, name: 'Pink Women Shorts', price: '$5.99'},
-         {id: 3, name: 'Nike white Shoes', price: '$300'},
-         {id: 4, name: 'Jimmy', price: '$100.99'}
-     ];
- 
-     // get database from event
-     var db = event.target.result;
- 
-     // create transaction from database
-     var transaction = db.transaction(["VocabDB"], 'readwrite');//more like giving access
-     //var transaction = db.createObjectStore('vocabProgress', {keyPath: 'id'});
-     // add success event handleer for transaction
-     // you should also add onerror, onabort event handlers
-     transaction.onsuccess = function(event) {
-         console.log('[Transaction] ALL DONE!');
-         console.log('erroorrrr!!');
-     };
- 
-     // get store from transaction
-     var productsStore = transaction.objectStore('VocabDB');
- 
-     /*************************************/
- 
-     // put products data in productsStore
-     vocabProgress.forEach(function(vocab){//this is like lambda function
-         var db_op_req = productsStore.add(vocab);
- 
-         db_op_req.onsuccess = function(event) {
-             console.log(event.target.result === vocab.id); // true
-             console.log('db transaction is true ');
-         }
-     });
- 
-     // count number of objects in store
-     productsStore.count().onsuccess = function(event) {
-         console.log('[Transaction - COUNT] number of products in store', event.target.result);
-     };
- 
-     // get product with id 1
-     productsStore.get(1).onsuccess = function(event) {
-         console.log('[Transaction - GET] product with id 1', event.target.result);
-     };
- 
-     // update product with id 1
-     vocabProgress[0].name = 'Blue Men T-shirt';
-     productsStore.put(vocabProgress[0]).onsuccess = function(event) {
-         console.log('[Transaction - PUT] product with id 1', event.target.result);
-     };
-     /*
-     // delete product with id 2
-     productsStore.delete(2).onsuccess = function(event) {
-         console.log('[Transaction - DELETE] deleted with id 2');
-     };
-      */
- };
- 
- request.onerror = function(event) {
-     console.log('[onerror]', request.error);
- };
- 
- request.onupgradeneeded = function(event) {
-    console.log('first db createion');
-     var db = event.target.result;
-     var productsStore = db.createObjectStore('VocabDB', {keyPath: 'id'});
- };
 
-console.log('productStore', productStore);
 
 class App extends React.Component {
-  
-
-
-
   //constructor sets the state
   constructor(props){
     super(props);
@@ -104,6 +28,75 @@ class App extends React.Component {
       words: [],
       list_words: [],
       voc_rage: 0
+    };
+
+    //db setup
+    var request = indexedDB.open('vocabDB', 1);
+
+    request.onupgradeneeded = function(event) {
+      console.log('first db createion');
+       var db = event.target.result;
+       var productsStore = db.createObjectStore('VocabDB', {keyPath: 'id'});
+   };
+   request.onsuccess = function(event) {
+    console.log('request done succesffullly');
+    // some sample products data
+    var vocabProgress = [
+        {id: 1, name: 'Red Men T-Shirt', price: '$3.99'},
+        {id: 2, name: 'Pink Women Shorts', price: '$5.99'},
+        {id: 3, name: 'Nike white Shoes', price: '$300'},
+        {id: 4, name: 'Jimmy', price: '$100.99'}
+    ];
+
+    // get database from event
+    var db = event.target.result;
+
+    // create transaction from database
+    var transaction = db.transaction(["VocabDB"], 'readwrite');//more like giving access
+    //var transaction = db.createObjectStore('vocabProgress', {keyPath: 'id'});
+    // add success event handleer for transaction
+    // you should also add onerror, onabort event handlers
+    transaction.onsuccess = function(event) {
+        console.log('[Transaction] ALL DONE!');
+        console.log('erroorrrr!!');
+    };
+
+    // get store from transaction
+    var productsStore = transaction.objectStore('VocabDB');
+
+    /*************************************/
+
+    // put products data in productsStore
+    vocabProgress.forEach(function(vocab){//this is like lambda function
+        var db_op_req = productsStore.add(vocab);
+
+        db_op_req.onsuccess = function(event) {
+            console.log(event.target.result === vocab.id); // true
+            console.log('db transaction is true ');
+        }
+    });
+
+    // count number of objects in store
+    productsStore.count().onsuccess = function(event) {
+        console.log('[Transaction - COUNT] number of products in store', event.target.result);
+    };
+
+    // get product with id 1
+    productsStore.get(1).onsuccess = function(event) {
+        console.log('[Transaction - GET] product with id 1', event.target.result);
+    };
+
+    // update product with id 1
+    vocabProgress[0].name = 'Blue Men T-shirt';
+    productsStore.put(vocabProgress[0]).onsuccess = function(event) {
+        console.log('[Transaction - PUT] product with id 1', event.target.result);
+    };
+    /*
+    // delete product with id 2
+    productsStore.delete(2).onsuccess = function(event) {
+        console.log('[Transaction - DELETE] deleted with id 2');
+    };
+     */
     };
   }
 
@@ -161,6 +154,17 @@ class App extends React.Component {
 
 
   render_views = page_state => {
+    var request = indexedDB.open('vocabDB', 1);
+    request.onsuccess = function(event){
+      var db = event.target.result;
+      var transaction = db.transaction(["VocabDB"], 'readwrite');
+      var productsStore = transaction.objectStore('VocabDB');
+      productsStore.get(1).onsuccess = function(event) {
+        console.log('[Transaction - GET] product with id 1', event.target.result);
+    };
+    }
+
+    console.log('request is : ',request);
     console.log('in renderviews: ', this.state.days);
     if(page_state === '1'){
       console.log('here')
